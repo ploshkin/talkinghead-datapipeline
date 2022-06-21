fa111966a5f6c98946b60a344d0e5d44e0042e2c
Alexander Ploshkin <alexploshkin@gmail.com>
2022-06-21 17:00:54 +0000

    Add landmarks3d to A2enDatasetNode

--- dpl/processor/nodes/a2en.py
+++ dpl/processor/nodes/a2en.py
@@ -16,6 +16,7 @@ class A2enDatasetNode(BaseNode):
         DataType.VOLUME,
         DataType.EXP,
         DataType.POSE,
+        DataType.LANDMARKS3D,
     ]
     output_types = [DataType.A2EN]
 
@@ -26,6 +27,9 @@ class A2enDatasetNode(BaseNode):
     ) -> None:
         data = {key: np.load(input_paths[key]) for key in ["wav2vec", "volume", "exp"]}
         data["jaw"] = np.load(input_paths["pose"])[:, 3]
+
+        landmarks3d = np.load(input_paths["landmarks3d"])
+        data.update(self.get_blinks_data(landmarks3d))
 
         fps = common.get_fps(input_paths["video"])
         num = len(data["exp"])
@@ -38,3 +42,12 @@ class A2enDatasetNode(BaseNode):
 
         output_paths["a2en"].parent.mkdir(parents=True, exist_ok=True)
         np.savez(output_paths["a2en"], **data)
+
+    def get_blinks_data(self, landmarks3d: np.ndarray) -> Dict[str, np.ndarray]:
+        def l2_lmk(i: int, j: int) -> np.ndarray:
+            return np.linalg.norm(landmarks3d[:, i] - landmarks3d[:, j], axis=1)
+
+        return {
+            "left_blink": (l2_lmk(37, 41) + l2_lmk(38, 40)) / (l2_lmk(36, 39) * 2),
+            "right_blink": (l2_lmk(43, 47) + l2_lmk(44, 46)) / (l2_lmk(42, 45) * 2),
+        }
